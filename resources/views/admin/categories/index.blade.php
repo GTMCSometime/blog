b4c2699bc3be5f7bb02d084ff65c606d8d9850d1
Alexandr_Shmidt <alexandr_shmidt98@vk.com>
2025-02-11 13:48:21 +0700

    now can watch to category

--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -4,7 +4,7 @@
       <div class="container-fluid">
         <!-- Small boxes (Stat box) -->
         <div class="row">
-        <div class="col-1">
+        <div class="col- mb-3">
             <a href="{{ route('admin.category.create')}}" class="btn btn-block btn-primary"> Добавить</a>
             </div>
           <div class="col-12">
@@ -12,6 +12,26 @@
             Категории
             </div>
         </div>
+        <div class="row">
+        <div class="col-6">
+        <table class="table">
+  <thead>
+    <tr>
+      <th>ID</th>
+      <th>Название</th>
+    </tr>
+  </thead>
+  <tbody>
+    @foreach ($categories as $category)
+    <tr>
+      <td>{{ $category->id}}</td>
+      <td>{{ $category->title}}</td>
+      @endforeach
+    </tr>
+  </tbody>
+</table>
+</div>
+</div> 
         </div>
     </section>
 @endsection
