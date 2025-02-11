056fea94d74431ed79b69c930597a993d846a6bf
Alexandr_Shmidt <alexandr_shmidt98@vk.com>
2025-02-11 13:23:44 +0700

    add store request. add filleable to Category model. update create.blade

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -9,6 +9,11 @@ class Category extends Model
 {
     use HasFactory;
 
+    protected $fillable = [
+        'title',
+    ];
+
+
     protected $table = 'categories';
     protected $guard = false;
     
--- resources/views/admin/categories/create.blade.php
+++ resources/views/admin/categories/create.blade.php
@@ -7,11 +7,16 @@
           <div class="col-12">
             <!-- small box -->
             <h6 class="mb-3">Добавление категории</h6>
-            <form action="#" class="w-25">
+            <form action="{{ route('admin.category.store' )}}" method="post" class="w-25">
+              @csrf
   <div class="mb-3">
     <label for="title" class="form-label">Название</label>
-    <input type="text" class="form-control" placeholder="Название категории">
+    <input type="text" class="form-control" name="title" placeholder="Название категории">
+    @error('title')
+<div class="text-danger">Это поле необходимо заполнить</div>
+  @enderror
   </div>
+
   <button type="submit" class="btn btn-primary" value="Добавить">Добавить</button>
 </form>
             </div>
