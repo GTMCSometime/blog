d69d4af89050f40be42c7aaf05e769a1e839d529
Alexandr_Shmidt <alexandr_shmidt98@vk.com>
2025-02-25 12:26:50 +0700

    add route from personal.main.index

--- resources/views/personal/main/index.blade.php
+++ resources/views/personal/main/index.blade.php
@@ -23,7 +23,7 @@
               <div class="icon">
                 <i class="far fa-heart"></i>
               </div>
-              <a href="#" class="small-box-footer">подробнее<i class="fas fa-arrow-circle-right"></i></a>
+              <a href="{{ route('personal.liked.index') }}" class="small-box-footer">подробнее  <i class="fas fa-arrow-circle-right"></i></a>
             </div>
           </div>
           <div class="col-lg-3 col-6">
@@ -36,7 +36,7 @@
               <div class="icon">
                 <i class="far fa-comments"></i>
               </div>
-              <a href="#" class="small-box-footer">подробнее<i class="fas fa-arrow-circle-right"></i></a>
+              <a href="{{ route('personal.comment.index') }}" class="small-box-footer">подробнее  <i class="fas fa-arrow-circle-right"></i></a>
             </div>
           </div>
       </div>
