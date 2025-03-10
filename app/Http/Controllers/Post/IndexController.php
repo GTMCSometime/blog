8f28f066708b3f1425ee7f2c9f12d53e7c10485c
Alexandr_Shmidt <alexandr_shmidt98@vk.com>
2025-03-10 18:36:40 +0700

    now use mysql

--- app/Http/Controllers/Post/IndexController.php
+++ app/Http/Controllers/Post/IndexController.php
@@ -8,11 +8,22 @@ use App\Models\Post;
 class IndexController extends Controller
 {
     public function __invoke() {
-     
         $posts = Post::paginate(6);
-        $randomPosts = Post::get()->random(4);
-        $likedPosts = Post::withCount('likedUser')->orderBy('liked_users_count', 'DESC')->get()->take(4);
+        if(Post::count() > 3) {
+            $randomPosts = Post::get()->random(4); 
+        } else {
+            $randomPosts = Post::get()->random(Post::count());
+        }
+
+        if(Post::withCount('likedUser')->orderBy('liked_users_count', 'DESC')->count() > 3) {
+            $likedPosts = Post::withCount('likedUser')->get()->take(4);
+        } else {
+            $likedPosts = Post::withCount('likedUser')->get()->random(Post::count());
+        }
+         
         return view('post.index', compact('posts', 'randomPosts', 'likedPosts'));
 
     }
 }
+
+
